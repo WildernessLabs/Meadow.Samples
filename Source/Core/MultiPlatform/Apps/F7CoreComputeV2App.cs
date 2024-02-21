c376832d3a9a16d3a9721ad770b59fbcd62abe34
Chris Tacke <ctacke@opennetcf.com>
2024-02-21 16:12:18 -0600

    added multi-platform sample

---
+++ Source/Core/MultiPlatform/Apps/F7CoreComputeV2App.cs
@@ -0,0 +1,32 @@
+﻿using Meadow;
+using Meadow.Devices;
+using System.Threading.Tasks;
+
+namespace MultiPlatformApp;
+
+public class F7CoreComputeV2App : App<F7CoreComputeV2>
+{
+    private OutputController outputController;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initializing F7CoreComputeV2 App...");
+
+        outputController = new OutputController(Device.Pins.D09);
+
+        return base.Initialize();
+    }
+
+    public override async Task Run()
+    {
+        Resolver.Log.Info("Running F7CoreComputeV2 App...");
+
+        while (true)
+        {
+            outputController.SetOutputState(true);
+            await Task.Delay(1000);
+            outputController.SetOutputState(false);
+            await Task.Delay(100);
+        }
+    }
+}
---
+++ Source/Core/MultiPlatform/OutputController.cs
@@ -0,0 +1,19 @@
+﻿using Meadow;
+using Meadow.Hardware;
+
+namespace MultiPlatformApp;
+
+public class OutputController
+{
+    private IDigitalOutputPort outputPort;
+
+    public OutputController(IPin outputPin)
+    {
+        outputPort = outputPin.CreateDigitalOutputPort();
+    }
+
+    public void SetOutputState(bool state)
+    {
+        outputPort.State = state;
+    }
+}
