76ad096b5b118ee0bf61332dea9c989d87a7a5f6
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-03-07 01:13:41 -0800

    Add project in Meadow.Desktop.Samples

---
+++ Source/Meadow.Desktop.Samples/Source/Touchscreen_Demo/MeadowApp.cs
@@ -0,0 +1,45 @@
+﻿using Meadow;
+using Meadow.Foundation.Displays;
+using Meadow.Foundation.Graphics.MicroLayout;
+using Meadow.Hardware;
+using System.Threading.Tasks;
+
+namespace Touchscreen_Demo;
+
+public class MeadowAppDesktop : App<Desktop>
+{
+    private CircleDemo _demo;
+
+    public override Task Initialize()
+    {
+        var touchscreen = Device.Display as ITouchScreen;
+        var display = new DisplayScreen(
+                    Device.Display,
+                    touchScreen: touchscreen);
+
+        _demo = new CircleDemo(display, touchscreen);
+
+        return base.Initialize();
+    }
+
+    public override Task Run()
+    {
+        _demo.Start();
+
+        // NOTE: this will not return until the display is closed
+        ExecutePlatformDisplayRunner();
+
+        return Task.CompletedTask;
+    }
+
+    private void ExecutePlatformDisplayRunner()
+    {
+#if WINDOWS
+        System.Windows.Forms.Application.Run(Device.Display as System.Windows.Forms.Form);
+#endif
+        if (Device.Display is GtkDisplay gtk)
+        {
+            gtk.Run();
+        }
+    }
+}
