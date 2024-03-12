ec3ae0a66fdbdec4d33297222039f07ce2ec5d48
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-03-12 01:54:32 -0700

    Update WiFiWeather app for Raspberry Pi

--- Source/Meadow.SBCs.Samples/Source/WifiWeather/MeadowApp.cs
+++ Source/Meadow.SBCs.Samples/Source/WifiWeather/MeadowApp.cs
@@ -1,66 +1,29 @@
 ﻿using Meadow;
-using Meadow.Foundation.Displays;
-using System;
 using System.Threading.Tasks;
-using WifiWeather.Services;
-using WifiWeather.ViewModels;
-using WifiWeather.Views;
 
 namespace WifiWeather;
 
 public class MeadowApp : LinuxApp<RaspberryPi>
 {
-    private DisplayView? _displayController;
+    MainController mainController;
 
     public override Task Initialize()
     {
-        Resolver.Log.Info("Creating Outputs");
+        Resolver.Log.Info("Initialize...");
 
-        var spiBus = Device.CreateSpiBus(
-            Device.Pins.SPI0_SCLK,
-            Device.Pins.SPI0_MOSI,
-            Device.Pins.SPI0_MISO,
-            new Meadow.Units.Frequency(48, Meadow.Units.Frequency.UnitType.Megahertz));
+        //var hardware = new WifiWeatherHardware();
+        //var network = Device.NetworkAdapters.Primary<INetworkAdapter>();
 
-        var display = new Ili9488
-        (
-            spiBus: spiBus,
-            chipSelectPin: Device.Pins.GPIO27,
-            dcPin: Device.Pins.GPIO27,
-            resetPin: Device.Pins.GPIO27
-        );
-
-        _displayController = new DisplayView();
-        _displayController.Initialize(display);
+        mainController = new MainController();
+        mainController.Initialize();
 
         return Task.CompletedTask;
     }
 
-    async Task GetTemperature()
-    {
-        // Get outdoor conditions
-        var outdoorConditions = await WeatherService.GetWeatherForecast();
-
-        // Format indoor/outdoor conditions data
-        var model = new WeatherViewModel(outdoorConditions);
-
-        // Send formatted data to display to render
-        _displayController.UpdateDisplay(model);
-    }
-
     public override async Task Run()
     {
-        await GetTemperature();
+        Resolver.Log.Info("Run...");
 
-        while (true)
-        {
-            if (DateTime.Now.Minute == 0 && DateTime.Now.Second == 0)
-            {
-                await GetTemperature();
-            }
-
-            _displayController.UpdateDateTime();
-            await Task.Delay(TimeSpan.FromSeconds(1));
-        }
+        await mainController.Run();
     }
 }
