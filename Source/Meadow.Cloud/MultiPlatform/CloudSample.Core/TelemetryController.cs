dd0b272c0a8de9f89d58d2bd854384cee1e63502
Chris Tacke <ctacke@opennetcf.com>
2024-04-01 10:59:21 -0500

    added RasPi file system info sample

---
+++ Source/Meadow.Cloud/MultiPlatform/CloudSample.Core/TelemetryController.cs
@@ -0,0 +1,29 @@
+﻿using Meadow.Cloud;
+using System;
+using System.Collections.Generic;
+
+namespace CloudSample;
+
+public class TelemetryController
+{
+    private const int DataEventId = 2000;
+
+    private Random random = new();
+    private IMeadowCloudService cloudService;
+
+    public TelemetryController(IMeadowCloudService cloudService)
+    {
+        this.cloudService = cloudService;
+    }
+
+    public void LogTelemetry()
+    {
+        var data = new Dictionary<string, object>
+        {
+            { "Int Value", random.Next(43) },
+            { "String Value", BitConverter.ToString(BitConverter.GetBytes(random.NextDouble())) }
+        };
+
+        cloudService.SendEvent(DataEventId, "CloudSample Data", data);
+    }
+}
