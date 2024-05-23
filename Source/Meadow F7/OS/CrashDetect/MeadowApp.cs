11afcd9dd248f445162d4ec4dfaac914ed5d2a3f
Chris Tacke <ctacke@opennetcf.com>
2024-05-23 10:48:33 -0500

    working on crash detect sample

--- Source/Meadow F7/OS/CrashDetect/MeadowApp.cs
+++ Source/Meadow F7/OS/CrashDetect/MeadowApp.cs
@@ -1,38 +1,105 @@
 ﻿using Meadow;
 using Meadow.Devices;
+using Meadow.Devices.Esp32.MessagePayloads;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CrashDetectTest;
 
-public class FeatherApp : App<F7FeatherV2> { }
-public class CoreComputeApp : App<F7CoreComputeV2> { }
+public class FeatherApp : MeadowApp<F7FeatherV2> { }
+public class CoreComputeApp : MeadowApp<F7CoreComputeV2> { }
 
 public class MeadowApp<T> : App<T>
     where T : F7MicroBase
 {
-    public override void OnBootFromCrash(IEnumerable<string> crashReports)
-    {
-        Resolver.Log.Info("Booting after a crash!");
+    private Random _random = new();
+    private bool _bootAfterCrash = false;
 
-        Resolver.Log.Info("Crash report:");
-        foreach (var r in crashReports)
+    public override Task Initialize()
+    {
+        var reliabilityService = Resolver.Services.Get<IReliabilityService>();
+
+        reliabilityService.MeadowSystemError += ReliabilityService_MeadowSystemError;
+
+        _bootAfterCrash = true;
+
+        if (reliabilityService.LastBootWasFromCrash)
         {
-            Resolver.Log.Info(r);
+            Resolver.Log.Info("Booting after a crash!");
+
+            Resolver.Log.Info("Crash report:");
+            foreach (var r in reliabilityService.GetCrashData())
+            {
+                Resolver.Log.Info(r);
+            }
+
+            Resolver.Log.Info("Clearing crash data...");
+            reliabilityService.ClearCrashData();
         }
 
-        Resolver.Log.Info("Clearing crash data...");
-        Resolver.Services.Get<CrashReporter>()?.ClearCrashData();
+        return base.Initialize();
+    }
+
+    private void ReliabilityService_MeadowSystemError(object sender, MeadowSystemErrorInfo e)
+    {
+        if (e is Esp32SystemErrorInfo espError)
+        {
+            Resolver.Log.Warn($"The ESP32 has had an error ({espError.StatusCode}).");
+
+            switch (espError.StatusCode)
+            {
+                case StatusCodes.EspOutOfMemory:
+                    Resolver.Log.Error($"This is a fatal error. Resetting the device...");
+                    Resolver.Device.PlatformOS.Reset();
+                    break;
+                default:
+                    // any ESP reset error code is also fatal
+                    if (espError.StatusCode >= StatusCodes.EspReset && espError.StatusCode <= StatusCodes.EspResetSDIO)
+                    {
+                        Resolver.Log.Error($"This is a fatal error. Resetting the device...");
+                        Resolver.Device.PlatformOS.Reset();
+                    }
+                    break;
+            }
+        }
+        else
+        {
+            Resolver.Log.Info($"We've had a system error: {e}");
+        }
     }
 
     public override async Task Run()
     {
-        var r = new Random();
+        if (!_bootAfterCrash)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(_random.Next(3, 20)));
+            Resolver.Log.Info("FORCING A CRASH!");
+            throw new Exception($"OMG! My App Died with a random code {_random.Next(1, 101)}");
+        }
+        else
+        {
+            Resolver.Log.Info("Waiting for an ESP reset");
 
-        await Task.Delay(TimeSpan.FromSeconds(r.Next(3, 20)));
+            //                CreateAnOOMError();
+        }
+    }
 
-        Resolver.Log.Info("FORCING A CRASH!");
-        throw new Exception($"OMG! My App Died with a random code {r.Next(1, 101)}");
+    private void CreateAnOOMError()
+    {
+        Resolver.Log.Info("Please wait.  Buffering....");
+        var list = new List<byte>();
+
+        var size = 1;
+
+        while (true)
+        {
+            var buffer = new byte[size];
+            _random.NextBytes(buffer);
+            list.AddRange(buffer);
+            size *= 2;
+            Thread.Sleep(100);
+        }
     }
 }
