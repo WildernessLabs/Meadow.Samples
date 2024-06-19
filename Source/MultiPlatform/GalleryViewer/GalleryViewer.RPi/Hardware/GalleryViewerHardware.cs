f75aac16f032a4e118afc28264a95eeedb8575ac
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-06-18 23:24:31 -0700

    Wired buttons on GalleryViewer RPi

--- Source/MultiPlatform/GalleryViewer/GalleryViewer.RPi/Hardware/GalleryViewerHardware.cs
+++ Source/MultiPlatform/GalleryViewer/GalleryViewer.RPi/Hardware/GalleryViewerHardware.cs
@@ -1,8 +1,11 @@
 ﻿using GalleryViewer.Core.Contracts;
 using Meadow;
 using Meadow.Foundation.Displays;
+using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Hardware;
 using Meadow.Peripherals.Displays;
 using Meadow.Peripherals.Sensors.Buttons;
+using Meadow.Units;
 
 namespace GalleryViewer.RPi.Hardware;
 
@@ -10,24 +13,30 @@ internal class GalleryViewerHardware : IGalleryViewerHardware
 {
     private readonly RaspberryPi device;
     private readonly IPixelDisplay? display = null;
+    private readonly IButton? leftButton = null;
+    private readonly IButton? rightButton = null;
+
+    public IButton? LeftButton => leftButton;
+
+    public IButton? RightButton => rightButton;
 
     public IPixelDisplay? Display => display;
 
     public RotationType DisplayRotation => RotationType._270Degrees;
 
-    public IButton? RightButton => null;
-
-    public IButton? LeftButton => null;
-
     public GalleryViewerHardware(RaspberryPi device)
     {
         this.device = device;
+
+        leftButton = new PushButton(device.Pins.GPIO26, ResistorMode.ExternalPullUp);
+
+        rightButton = new PushButton(device.Pins.GPIO19, ResistorMode.ExternalPullDown);
 
         var spiBus = device.CreateSpiBus(
             device.Pins.SPI0_SCLK,
             device.Pins.SPI0_MOSI,
             device.Pins.SPI0_MISO,
-            new Meadow.Units.Frequency(48, Meadow.Units.Frequency.UnitType.Megahertz));
+            new Frequency(48, Frequency.UnitType.Megahertz));
 
         display = new Ili9341
         (
