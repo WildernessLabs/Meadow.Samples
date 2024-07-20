9f884b070620e045d32dc26014b4f935f5540bc6
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-07-19 23:20:20 -0700

    Add ProjectLab Cellular sample

---
+++ Source/ProjectLab/CellularSample/DisplayController.cs
@@ -0,0 +1,110 @@
+﻿using Meadow;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Graphics.MicroLayout;
+using Meadow.Peripherals.Displays;
+using System;
+using System.Threading.Tasks;
+
+namespace CellularSample;
+
+public class DisplayController
+{
+    DisplayScreen screen;
+
+    private readonly Image imgSignal0Bar = Image.LoadFromResource("CellularSample.Assets.img-cell-0.bmp");
+    private readonly Image imgSignal1Bar = Image.LoadFromResource("CellularSample.Assets.img-cell-1.bmp");
+    private readonly Image imgSignal2Bar = Image.LoadFromResource("CellularSample.Assets.img-cell-2.bmp");
+    private readonly Image imgSignal3Bar = Image.LoadFromResource("CellularSample.Assets.img-cell-3.bmp");
+    private readonly Image imgSignal4Bar = Image.LoadFromResource("CellularSample.Assets.img-cell-4.bmp");
+
+    private Label status;
+    private Label ipAddress;
+    private Picture signalBars;
+
+    public DisplayController(IPixelDisplay _display)
+    {
+        screen = new DisplayScreen(_display, RotationType._270Degrees)
+        {
+            BackgroundColor = Color.FromHex("14607F")
+        };
+
+        screen.Controls.Add(new Box(5, 5, screen.Width - 10, screen.Height - 10)
+        {
+            IsFilled = false,
+            ForeColor = Color.FromHex("F9E000")
+        });
+
+        signalBars = new Picture(105, 33, 110, 103, imgSignal0Bar);
+        screen.Controls.Add(signalBars);
+
+        status = new Label(60, 149, 200, 24)
+        {
+            Text = "OFFLINE...",
+            TextColor = Color.FromHex("F9E000"),
+            Font = new Font16x24(),
+            HorizontalAlignment = HorizontalAlignment.Center,
+        };
+        screen.Controls.Add(status);
+
+        ipAddress = new Label(35, 183, 250, 28)
+        {
+            Text = "---.---.---.---",
+            TextColor = Color.FromHex("14607F"),
+            Font = new Font16x24(),
+            BackColor = Color.FromHex("F9E000"),
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Bottom,
+        };
+        screen.Controls.Add(ipAddress);
+    }
+
+    public void UpdateSignalBar(int strength)
+    {
+        Resolver.Log.Info("Signal Strength: " + strength);
+
+        switch (strength)
+        {
+            case int n when (n >= -70 && n <= -50):
+                signalBars.Image = imgSignal4Bar;
+                break;
+            case int n when (n >= -80 && n <= -71):
+                signalBars.Image = imgSignal3Bar;
+                break;
+            case int n when (n >= -90 && n <= -81):
+                signalBars.Image = imgSignal2Bar;
+                break;
+            case int n when (n >= -100 && n <= -91):
+                signalBars.Image = imgSignal1Bar;
+                break;
+            default:
+                signalBars.Image = imgSignal0Bar;
+                break;
+        }
+    }
+
+    public void UpdateStatus(string status)
+    {
+        this.status.Text = status;
+    }
+
+    public void UpdateIpAddress(string ipAddress)
+    {
+        this.ipAddress.Text = string.IsNullOrEmpty(ipAddress)
+            ? "---.---.---.---"
+            : ipAddress;
+    }
+
+    public async Task Run()
+    {
+        bool status = false;
+
+        while (true)
+        {
+            var random = new Random();
+
+            status = !status;
+
+            await Task.Delay(1000);
+        }
+    }
+}
---
+++ Source/ProjectLab/CellularSample/MeadowApp.cs
@@ -0,0 +1,129 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Hardware;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CellularSample;
+
+public class MeadowApp : ProjectLabCoreComputeApp
+{
+    private DisplayController? displayController;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initialize...");
+
+        var cell = Hardware.ComputeModule.NetworkAdapters.Primary<ICellNetworkAdapter>();
+        cell.NetworkDisconnected += CellAdapter_NetworkDisconnected;
+        cell.NetworkConnected += CellAdapter_NetworkConnected;
+
+        Resolver.Log.Info($"Running on ProjectLab Hardware {Hardware.RevisionString}");
+
+        if (Hardware.RgbLed is { } rgbLed)
+        {
+            rgbLed.SetColor(Color.Blue);
+        }
+
+        if (Hardware.Display is { } display)
+        {
+            displayController = new DisplayController(display);
+
+            displayController.UpdateSignalBar(cell.GetSignalQuality());
+            displayController.UpdateStatus(cell.IsConnected ? "CONNECTED" : "DISCONNECTED");
+            displayController.UpdateIpAddress(cell.IsConnected ? cell.IpAddress.ToString() : "---.---.---.---");
+        }
+
+        return Task.CompletedTask;
+    }
+
+    void CellAdapter_NetworkDisconnected(INetworkAdapter sender, NetworkDisconnectionEventArgs args)
+    {
+        displayController.UpdateStatus("DISCONNECTED");
+        displayController.UpdateIpAddress("---.---.---.---");
+        displayController.UpdateSignalBar(-9999);
+    }
+
+    async void CellAdapter_NetworkConnected(INetworkAdapter networkAdapter, NetworkConnectionEventArgs e)
+    {
+        var cell = networkAdapter as ICellNetworkAdapter;
+
+        if (cell != null)
+        {
+            Resolver.Log.Info("Cell CSQ at the time of connection (dbm): " + cell.Csq);
+            Resolver.Log.Info("Cell IMEI: " + cell.Imei);
+
+            displayController.UpdateStatus("CONNECTED");
+            displayController.UpdateIpAddress(cell.IpAddress.ToString());
+            displayController.UpdateSignalBar(cell.Csq);
+
+            await GetWebPageViaHttpClient("https://postman-echo.com/get?fool=bar1&foo2=bar2");
+        }
+    }
+
+    async Task GetWebPageViaHttpClient(string uri)
+    {
+        Console.WriteLine($"Requesting {uri} - {DateTime.Now}");
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        using (HttpClient client = new HttpClient())
+        {
+            // In weak signal connections and/or large download scenarios, it's recommended to increase the client timeout
+            client.Timeout = TimeSpan.FromMinutes(5);
+            using (HttpResponseMessage response = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead))
+            {
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var contentLength = response.Content.Headers.ContentLength ?? -1L;
+                    var progress = new Progress<long>(totalBytes =>
+                    {
+                        Console.WriteLine($"{totalBytes} bytes downloaded ({(double)totalBytes / contentLength:P2})");
+                    });
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        var buffer = new byte[4096];
+                        long totalBytesRead = 0;
+                        int bytesRead;
+
+                        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            totalBytesRead += bytesRead;
+                            ((IProgress<long>)progress).Report(totalBytesRead);
+                        }
+                    }
+
+                    stopwatch.Stop();
+                    Console.WriteLine($"Download complete. Time taken: {stopwatch.Elapsed.TotalSeconds:F2} seconds");
+                }
+                catch (TaskCanceledException)
+                {
+                    Console.WriteLine("Request timed out.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Request went sideways: {e.Message}");
+                }
+            }
+        }
+    }
+
+    public override Task Run()
+    {
+        Resolver.Log.Info("Run...");
+
+        if (Hardware?.RgbLed is { } rgbLed)
+        {
+            Resolver.Log.Info("starting blink");
+            _ = rgbLed.StartBlink(WildernessLabsColors.PearGreen, TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(2000), 0.5f);
+        }
+
+        return Task.CompletedTask;
+    }
+}
