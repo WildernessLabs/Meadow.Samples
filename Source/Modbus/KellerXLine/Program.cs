99698b677a05e86b239664c7392b7d7aad2aa04d
Chris Tacke <ctacke@opennetcf.com>
2025-01-06 17:42:59 -0600

    added a couple modbus samples

---
+++ Source/Modbus/KellerXLine/Program.cs
@@ -0,0 +1,36 @@
+﻿using Meadow.Foundation.Sensors.Environmental;
+using Meadow.Hardware;
+using Meadow.Modbus;
+
+namespace KellerXLine_Sample;
+
+internal class Program
+{
+    private static async Task Main(string[] _)
+    {
+        await Test();
+    }
+
+    private static async Task Test()
+    {
+
+        var serialPort = "COM12";
+
+        using (var port = new SerialPortShim(serialPort, KellerTransducer.DefaultBaudRate, Parity.None, 8, StopBits.One))
+        {
+            port.ReadTimeout = TimeSpan.FromSeconds(15);
+            port.Open();
+
+            var client = new ModbusRtuClient(port);
+            var sensor = new KellerTransducer(client, KellerTransducer.DefaultModbusAddress);
+
+            while (true)
+            {
+                var pressure = await sensor.ReadPressure(PressureChannel.P1);
+                Console.WriteLine($"Pressure: {pressure.Millibar} mbar");
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ Source/ProjectLab/KellerXLine/MeadowApp.cs
@@ -0,0 +1,43 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Environmental;
+using System;
+using System.Threading.Tasks;
+
+namespace KellerXLine_Sample;
+
+// Change ProjectLabCoreComputeApp to ProjectLabFeatherApp for ProjectLab v2
+public class MeadowApp : ProjectLabCoreComputeApp
+{
+    private IKellerTransducer sensor;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initialize...");
+
+        Resolver.Log.Info($"Running on ProjectLab Hardware {Hardware.RevisionString}");
+
+        var client = Hardware.GetModbusRtuClient(KellerTransducer.DefaultBaudRate);
+        sensor = new KellerTransducer(client, KellerTransducer.DefaultModbusAddress);
+
+        return base.Initialize();
+    }
+
+    public override async Task Run()
+    {
+        while (true)
+        {
+            try
+            {
+                var pressure = await sensor.ReadPressure(PressureChannel.P1);
+                Resolver.Log.Info($"Pressure: {pressure.Millibar} mbar");
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Info($"Error: {ex.Message}");
+            }
+
+            await Task.Delay(1000);
+        }
+    }
+}
